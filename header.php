fbd71ed863ad9575b1efee94d018a245f8904d11
kmike-wachira <mwkariuki2e@gmail.com>
2021-05-23 09:50:45 +0300

    included footer and headers

---
+++ header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Sogo Hotel by Colorlib.com</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="" />
+    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=|Roboto+Sans:400,700|Playfair+Display:400,700">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="css/jquery.timepicker.css">
+    <link rel="stylesheet" href="css/fancybox.min.css">
+
+    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+    <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<header class="site-header js-site-header">
+        <div class="container-fluid">
+            <div class="row align-items-center">
+                <div class="col-6 col-lg-4 site-logo" data-aos="fade">
+                    <a href="index.php">Make Up Class Booker </a>
+                </div>
+                <div class="col-6 col-lg-8">
+                    <div class="site-menu-toggle js-site-menu-toggle" data-aos="fade">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    <!-- END menu-toggle -->
+
+                    <div class="site-navbar js-site-navbar">
+                        <nav role="navigation">
+                            <div class="container">
+                                <div class="row full-height align-items-center">
+                                    <div class="col-md-6 mx-auto">
+                                        <ul class="list-unstyled menu">
+                                            <li class="active"><a href="index.php">Home</a></li>
+                                            <li><a href="login.php">Login</a></li>
+                                            <li><a href="register.php">Register</a></li>
+                                            <li><a href="addlectureroom.php">Add Halls</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
--- index.php
+++ index.php
@@ -1,251 +1,96 @@
-<?php include 'backend/functions.php'; ?>
-<!DOCTYPE HTML>
-<html>
+<?php include 'backend/functions.php';
+include 'header.php' ?>
+<!-- END head -->
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>LH Booker </title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <meta name="author" content="" />
-    <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=|Roboto+Sans:400,700|Playfair+Display:400,700">
-
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/aos.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
-    <link rel="stylesheet" href="css/fancybox.min.css">
-
-    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
-    <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
-
-    <!-- Theme Style -->
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-    <header class="site-header js-site-header">
-        <div class="container-fluid">
-            <div class="row align-items-center">
-                <div class="col-6 col-lg-4 site-logo" data-aos="fade">
-                    <a href="index.php">Room Booker </a>
-                </div>
-                <div class="col-6 col-lg-8">
-                    <div class="site-menu-toggle js-site-menu-toggle" data-aos="fade">
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                    </div>
-                    <!-- END menu-toggle -->
-
-                    <div class="site-navbar js-site-navbar">
-                        <nav role="navigation">
-                            <div class="container">
-                                <div class="row full-height align-items-center">
-                                    <div class="col-md-6 mx-auto">
-                                        <ul class="list-unstyled menu">
-                                            <li class="active"><a href="index.php">Home</a></li>
-                                            <li><a href="rooms.html">Rooms</a></li>
-                                            <li><a href="about.html">About</a></li>
-                                            <li><a href="events.html">Events</a></li>
-                                            <li><a href="contact.html">Contact</a></li>
-                                            <li><a href="reservation.html">Reservation</a></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </nav>
-                    </div>
-                </div>
+<section class="site-hero overlay" style="background-image: url(images/hero_4.jpg)" data-stellar-background-ratio="0.5">
+    <div class="container">
+        <div class="row site-hero-inner justify-content-center align-items-center">
+            <div class="col-md-10 text-center" data-aos="fade-up">
+                <span class="custom-caption text-uppercase text-white d-block  mb-3">Welcome To LH Booker<span class="fa fa-star text-primary"></span></span>
+                <h6 class="heading">Book a lecture hall for your students with litle hustle</h6>
             </div>
         </div>
-    </header>
-    <!-- END head -->
+    </div>
 
-    <section class="site-hero overlay" style="background-image: url(images/hero_4.jpg)" data-stellar-background-ratio="0.5">
-        <div class="container">
-            <div class="row site-hero-inner justify-content-center align-items-center">
-                <div class="col-md-10 text-center" data-aos="fade-up">
-                    <span class="custom-caption text-uppercase text-white d-block  mb-3">Welcome To LH Booker<span class="fa fa-star text-primary"></span></span>
-                    <h6 class="heading">Book a lecture hall for your students with litle hustle</h6>
-                </div>
-            </div>
+    <a class="mouse smoothscroll" href="#next">
+        <div class="mouse-icon">
+            <span class="mouse-wheel"></span>
         </div>
+    </a>
+</section>
+<!-- END section -->
 
-        <a class="mouse smoothscroll" href="#next">
-            <div class="mouse-icon">
-                <span class="mouse-wheel"></span>
-            </div>
-        </a>
-    </section>
-    <!-- END section -->
+<section class="section bg-light pb-0">
+    <div class="container">
 
-    <section class="section bg-light pb-0">
-        <div class="container">
+        <div class="row check-availabilty" id="next">
+            <div class="block-32" data-aos="fade-up" data-aos-offset="-200">
 
-            <div class="row check-availabilty" id="next">
-                <div class="block-32" data-aos="fade-up" data-aos-offset="-200">
-
-                    <form action="/backend/functions.php" method="post">
-                        <div class="row">
-                            <div class="col-md-4 mb-3 mb-lg-0 col-lg-3">
-                                <label for="checkin_date" class="font-weight-bold text-black">Lecture Date</label>
-                                <div class="field-icon-wrap">
-                                    <div class="icon"><span class="icon-calendar"></span></div>
-                                    <input type="date" id="checkin_date" name="lecture_date" class="form-control">
-                                </div>
-                            </div>
-                            <div class="col-md-2 mb-3 mb-md-0">
-                                <label for="adults" class="font-weight-bold text-black">Lecture periods</label>
-                                <div class="field-icon-wrap">
-                                    <div class="icon"><span class="ion-ios-arrow-down"></span></div>
-                                    <select name="lecture_period" id="adults" class="form-control">
-                                        <option value="1">7-9 am</option>
-                                        <option value="2">9-11 am</option>
-                                        <option value="3">11-13pm</option>
-                                        <option value="4">2pm-4pm</option>
-                                        <option value="5">4pm-6pm</option>
-                                    </select>
-                                </div>
-                            </div>
-
-                            <div class="col-md-4 mb-3 mb-lg-0 col-lg-3">
-                                <label for="checkin_date" class="font-weight-bold text-black">No Of Students</label>
-                                <div class="field-icon-wrap">
-                                    <div class="icon"><span class="icon-calendar"></span></div>
-                                    <input type="number" name="no_of_students" id="max-students" class="form-control w-100">
-                                </div>
-                            </div>
-                            <div class="col-md-4 col-lg-3 align-self-end">
-                                <button class="btn btn-primary btn-block text-white" type="submit" name="check">Check Availabilty</button>
+                <form action="/backend/functions.php" method="post">
+                    <div class="row">
+                        <div class="col-md-4 mb-3 mb-lg-0 col-lg-3">
+                            <label for="checkin_date" class="font-weight-bold text-black">Lecture Date</label>
+                            <div class="field-icon-wrap">
+                                <div class="icon"><span class="icon-calendar"></span></div>
+                                <input type="date" id="checkin_date" name="lecture_date" class="form-control">
                             </div>
                         </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
-    <section class="section blog-post-entry bg-light">
-        <div class="container">
-            <div class="row justify-content-center text-center mb-5">
-                <div class="col-md-7">
-                    <h2 class="heading" data-aos="fade-up">Institution Lecture Halls</h2>
-                    <p data-aos="fade-up"> Select the lecture hall that matches your needs</p>
-                </div>
-            </div>
-            <div class="row">
-                <?php
-                $lecturehalls = getAllLectureHalls();
-                foreach ($lecturehalls as $lecturehall) : ?>
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12 post" data-aos="fade-up" data-aos-delay="100">
-                        <div class="media media-custom d-block mb-4 h-100">
-                            <a href="#" class="mb-4 d-block"><img src="uploadimages/<?php echo $lecturehall['lh_cover_image'] ?>" alt="Image placeholder" class="img-fluid"></a>
-                            <div class="media-body">
-                                <!-- <span class="meta-post">February 26, 2018</span> -->
-                                <h2 class="mt-0 mb-3"><a href="/reservation.php?id=<?php echo $lecturehall['id'] ?>"><?php echo $lecturehall['lh_name'] ?></a></h2>
-                                <span class="meta-post"> Maximum No of Students <strong><?php echo $lecturehall['lh_capacity'] ?> </strong> </span>
-                                <h4>Brief</h4>
-                                <p><?php echo $lecturehall['lh_desc'] ?> </p>
+                        <div class="col-md-2 mb-3 mb-md-0">
+                            <label for="adults" class="font-weight-bold text-black">Lecture periods</label>
+                            <div class="field-icon-wrap">
+                                <div class="icon"><span class="ion-ios-arrow-down"></span></div>
+                                <select name="lecture_period" id="adults" class="form-control">
+                                    <option value="1">7-9 am</option>
+                                    <option value="2">9-11 am</option>
+                                    <option value="3">11-13pm</option>
+                                    <option value="4">2pm-4pm</option>
+                                    <option value="5">4pm-6pm</option>
+                                </select>
                             </div>
                         </div>
-                    </div>
-                <?php endforeach ?>
-            </div>
-        </div>
-    </section>
-    <!-- <section class="section bg-image overlay" style="background-image: url('images/hero_4.jpg');">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-12 col-md-6 text-center mb-4 mb-md-0 text-md-left" data-aos="fade-up">
-                    <h2 class="text-white font-weight-bold">A Best Place To Stay. Reserve Now!</h2>
-                </div>
-                <div class="col-12 col-md-6 text-center text-md-right" data-aos="fade-up" data-aos-delay="200">
-                    <a href="reservation.html" class="btn btn-outline-white-primary py-3 text-white px-5">Reserve Now</a>
-                </div>
-            </div>
-        </div>
-    </section> -->
 
-    <footer class="section footer-section">
-        <div class="container">
-            <div class="row mb-4">
-                <div class="col-md-3 mb-5">
-                    <ul class="list-unstyled link">
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Terms &amp; Conditions</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Rooms</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-3 mb-5">
-                    <ul class="list-unstyled link">
-                        <li><a href="#">The Rooms &amp; Suites</a></li>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Contact Us</a></li>
-                        <li><a href="#">Restaurant</a></li>
-                    </ul>
-                </div>
-                <div class="col-md-3 mb-5 pr-md-5 contact-info">
-                    <!-- <li>198 West 21th Street, <br> Suite 721 New York NY 10016</li> -->
-                    <p><span class="d-block"><span class="ion-ios-location h5 mr-3 text-primary"></span>Address:</span> <span> 198
-                            West 21th Street, <br> Suite 721 New York NY 10016</span></p>
-                    <p><span class="d-block"><span class="ion-ios-telephone h5 mr-3 text-primary"></span>Phone:</span> <span> (+1)
-                            435 3533</span></p>
-                    <p><span class="d-block"><span class="ion-ios-email h5 mr-3 text-primary"></span>Email:</span> <span>
-                            info@domain.com</span></p>
-                </div>
-                <div class="col-md-3 mb-5">
-                    <p>Sign up for our newsletter</p>
-                    <form action="#" class="footer-newsletter">
-                        <div class="form-group">
-                            <input type="email" class="form-control" placeholder="Email...">
-                            <button type="submit" class="btn"><span class="fa fa-paper-plane"></span></button>
+                        <div class="col-md-4 mb-3 mb-lg-0 col-lg-3">
+                            <label for="checkin_date" class="font-weight-bold text-black">No Of Students</label>
+                            <div class="field-icon-wrap">
+                                <div class="icon"><span class="icon-calendar"></span></div>
+                                <input type="number" name="no_of_students" id="max-students" class="form-control w-100">
+                            </div>
                         </div>
-                    </form>
-                </div>
-            </div>
-            <div class="row pt-5">
-                <p class="col-md-6 text-left">
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                    Copyright &copy;
-                    <script>
-                        document.write(new Date().getFullYear());
-                    </script> All rights reserved | This template is made with <i class="icon-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                    <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                </p>
-
-                <p class="col-md-6 text-right social">
-                    <a href="#"><span class="fa fa-tripadvisor"></span></a>
-                    <a href="#"><span class="fa fa-facebook"></span></a>
-                    <a href="#"><span class="fa fa-twitter"></span></a>
-                    <a href="#"><span class="fa fa-linkedin"></span></a>
-                    <a href="#"><span class="fa fa-vimeo"></span></a>
-                </p>
+                        <div class="col-md-4 col-lg-3 align-self-end">
+                            <button class="btn btn-primary btn-block text-white" type="submit" name="check">Check Availabilty</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
-    </footer>
-
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
-
-
-    <script src="js/aos.js"></script>
-
-    <script src="js/bootstrap-datepicker.js"></script>
-    <script src="js/jquery.timepicker.min.js"></script>
-
-
-
-    <script src="js/main.js"></script>
-</body>
-
-</html>
+    </div>
+</section>
+<section class="section blog-post-entry bg-light">
+    <div class="container">
+        <div class="row justify-content-center text-center mb-5">
+            <div class="col-md-7">
+                <h2 class="heading" data-aos="fade-up">Institution Lecture Halls</h2>
+                <p data-aos="fade-up"> Select the lecture hall that matches your needs</p>
+            </div>
+        </div>
+        <div class="row">
+            <?php
+            $lecturehalls = getAllLectureHalls();
+            foreach ($lecturehalls as $lecturehall) : ?>
+                <div class="col-lg-4 col-md-6 col-sm-6 col-12 post" data-aos="fade-up" data-aos-delay="100">
+                    <div class="media media-custom d-block mb-4 h-100">
+                        <a href="#" class="mb-4 d-block"><img src="uploadimages/<?php echo $lecturehall['lh_cover_image'] ?>" alt="Image placeholder" class="img-fluid"></a>
+                        <div class="media-body">
+                            <!-- <span class="meta-post">February 26, 2018</span> -->
+                            <h2 class="mt-0 mb-3"><a href="/reservation.php?id=<?php echo $lecturehall['id'] ?>"><?php echo $lecturehall['lh_name'] ?></a></h2>
+                            <span class="meta-post"> Maximum No of Students <strong><?php echo $lecturehall['lh_capacity'] ?> </strong> </span>
+                            <h4>Brief</h4>
+                            <p><?php echo $lecturehall['lh_desc'] ?> </p>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        </div>
+    </div>
+</section>
+<?php include 'footer.php' ?>
